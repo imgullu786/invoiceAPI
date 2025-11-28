c5dc257e54a0c248899836b9278c635e44c47a21
imgullu786 <123100963+imgullu786@users.noreply.github.com>
2025-11-28 20:14:24 +0530

    feat: dashboard, customer, items page added

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views_bp = Blueprint("views", __name__)
 
@@ -16,3 +17,18 @@ def login_page():
 @views_bp.route("/register")
 def register_page():
     return render_template("register.html")
+
+
+@views_bp.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@views_bp.route("/customers-ui")
+def customers_ui():
+    return render_template("customers.html")
+
+
+@views_bp.route("/items-ui")
+def items_ui():
+    return render_template("items.html")
